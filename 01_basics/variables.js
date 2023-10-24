75fc9f8025162590a1f807c417c40b67cc387f58
116reporting <133892552+116reporting@users.noreply.github.com>
2023-10-24 15:17:49 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId=433322
+let accountEmail= "mohit@google.com"
+var accountPassword = "12345"
+accountCity="jaipur"
+let accountstate="uttar pradesh"
+
+accountEmail="hc@hc.com"
+accountPassword="43434231121"
+accountCity="bengalaru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
